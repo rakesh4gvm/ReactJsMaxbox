9079c786d0ae49085c663e72e514a9c614b06aa6
shubhamgvm <shubham4gvm@gmail.com>
2022-08-18 18:28:46 +0530

    Add Notification in HeaderPage
    Add Notification in HeaderPage

--- src/_components/Header/header.js
+++ src/_components/Header/header.js
@@ -2,8 +2,7 @@ import React, { useRef, useState, useEffect } from 'react';
 import Axios from "axios";
 
 import { Select } from '@material-ui/core';
-import { Col, Container, Form, Nav } from 'react-bootstrap';
-import Button from '@mui/material/Button';
+import { Alert, Stack } from '@mui/material';
 import Navbar from 'react-bootstrap/Navbar'
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Mlogo from "../../images/Logo.png";
@@ -13,16 +12,12 @@ import Divider from '@mui/material/Divider';
 import ListItemText from '@mui/material/ListItemText';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
 import Avatar from '@mui/material/Avatar';
-
 import NotificationsIcon from '@material-ui/icons/Notifications';
 import MenuItem from '@material-ui/core/MenuItem';
 
-
 import allusers from '../../images/all_users.svg';
-import inboxuser1 from '../../images/avatar/1.jpg';
 import defaultimage from '../../images/default.png';
 import iconlogout from '../../images/icon_logout.svg';
-
 import inbox from '../../images/icons/inbox.svg';
 import menustart from '../../images/icons/menustart.svg';
 import drafts from '../../images/icons/drafts.svg';
@@ -33,14 +28,14 @@ import Chatmail from '../../images/icons/chat-mail.svg';
 import Sent from '../../images/icons/sent.svg';
 import Objectroup from '../../images/icons/object-group.svg';
 import Carbontem from '../../images/icons/carbontem.svg';
-
+import { Nav } from 'react-bootstrap';
 import { history } from "../../_helpers";
 import { CommonConstants } from "../../_constants/common.constants"
 import { ResponseMessage } from "../../_constants/response.message";
 import { UpdateUserDetails, GetUserDetails, Logout } from '../../_helpers/Utility'
 
 
-function useOutsideAlerter(ref) {
+function UseOutsideAlerter(ref) {
     useEffect(() => {
         function handleClickOutside(event) {
             if (ref.current && !ref.current.contains(event.target)) {
@@ -55,26 +50,34 @@ function useOutsideAlerter(ref) {
     }, [ref]);
 }
 
-
 export default function Header() {
-    const wrapperRef = useRef(null);
-    useOutsideAlerter(wrapperRef);
-
     const [ClientDropdown, SetClientDropdown] = useState([])
     const [SelectedClient, SetSelectedClient] = useState([]);
     const [UserImage, SetUserImage] = useState()
     const [UserDetails, SetUserDetails] = useState();
+    const [Show, SetShow] = useState(true)
+
+    useEffect(() => {
+        const TimeID = setTimeout(() => {
+            SetShow(false)
+        }, 3000)
+        return () => {
+            clearTimeout(TimeID)
+        }
+    }, []);
 
     useEffect(() => {
         GetClientDropdown()
     }, [SetSelectedClient])
 
+    // Select Client
     const SelectClient = (e) => {
         SetSelectedClient(e.target.value)
         UpdateUserDetails(e.target.value)
         window.location.reload(false);
     }
 
+    // Get Client Dropdown
     const GetClientDropdown = () => {
         var UserID
         var Details = GetUserDetails();
@@ -82,22 +85,21 @@ export default function Header() {
             UserID = Details.UserID
             SetUserImage(Details.UserImage)
         }
-        var data = {
+        var Data = {
             UserID: UserID,
         }
-        const responseapi = Axios({
+        const ResponseApi = Axios({
             url: CommonConstants.MOL_APIURL + "/client/GetClientListForTopDropDown",
             method: "POST",
-            data: data,
+            data: Data,
         });
-        responseapi.then((result) => {
-            if (result.data.StatusMessage == ResponseMessage.SUCCESS) {
-                if (result.data.Data.length > 0) {
-                    SetClientDropdown(result.data.Data);
+        ResponseApi.then((Result) => {
+            if (Result.data.StatusMessage == ResponseMessage.SUCCESS) {
+                if (Result.data.Data.length > 0) {
+                    SetClientDropdown(Result.data.Data);
                     if (Details.ClientID == null) {
-                        UpdateUserDetails((result.data.Data[0].ClientID))
-
-                        SetSelectedClient(result.data.Data[0]._id)
+                        UpdateUserDetails((Result.data.Data[0].ClientID))
+                        SetSelectedClient(Result.data.Data[0]._id)
                     }
                     else {
                         SetSelectedClient(Details.ClientID)
@@ -107,45 +109,50 @@ export default function Header() {
         });
     }
 
+    // Open Pop User Details
     const OpenPopupUserDetails = () => {
-        debugger;
         var UserID
         var Details = GetUserDetails();
         if (Details != null) {
             UserID = Details.UserID
         }
-        var data = {
+        var Data = {
             UserID: UserID,
         }
 
         const ResponseApi = Axios({
             url: CommonConstants.MOL_APIURL + "/user/AccountUserDetailsGetByID",
             method: "POST",
-            data: data,
+            data: Data,
         });
-        ResponseApi.then((result) => {
-            debugger;
-            if (result.data.StatusMessage == ResponseMessage.SUCCESS) {
-                if (result.data.Data.length >0) {
-                    SetUserDetails(result.data.Data[0])
-                    const element = document.getElementById("id_userbox")
-                    if (element.classList.contains("show")) {
-                        element.classList.remove("show");
+        ResponseApi.then((Result) => {
+            if (Result.data.StatusMessage == ResponseMessage.SUCCESS) {
+                if (Result.data.Data.length > 0) {
+                    SetUserDetails(Result.data.Data[0])
+                    const Element = document.getElementById("id_userbox")
+                    if (Element.classList.contains("show")) {
+                        Element.classList.remove("show");
                     }
                     else {
-                        element.classList.add("show");
+                        Element.classList.add("show");
                     }
-
                 }
             }
         });
     }
+
+    // SignOut
     const SignOut = () => {
         Logout();
     }
+
+    // OPen Pge
     const OpenPage = (PageName) => {
         history.push(PageName);
     }
+
+    const WrapperRef = useRef(null);
+    UseOutsideAlerter(WrapperRef);
 
     return (
         <>
@@ -156,7 +163,6 @@ export default function Header() {
                             <img src={Mlogo} />
                         </Navbar.Brand>
                     </div>
-
                     <div className='menulist right'>
                         <Navbar.Toggle aria-controls="basic-navbar-nav" />
                         <Navbar.Collapse id="basic-navbar-nav" className='mobile-nav'>
@@ -194,7 +200,6 @@ export default function Header() {
                                         </div>
                                     </NavDropdown.Item>
                                 </NavDropdown>
-
                                 <NavDropdown title="Sent" id="basic-nav-dropdown">
                                     <NavDropdown.Item href="#action/4.1">
                                         <img src={Chatmail} />Unanswered Replies
@@ -208,19 +213,14 @@ export default function Header() {
                                         </div>
                                     </NavDropdown.Item>
                                 </NavDropdown>
-
-
                                 <NavDropdown title="Templates" id="basic-nav-dropdown">
                                     <NavDropdown.Item href="#action/5.1">
                                         <img src={Objectroup} />Objections
-
                                     </NavDropdown.Item>
                                     <NavDropdown.Item href="/Templates">
                                         <img src={Carbontem} />Templates
-
                                     </NavDropdown.Item>
                                 </NavDropdown>
-
                                 <NavDropdown title="Settings" id="basic-nav-dropdown">
                                     <NavDropdown.Item onClick={() => OpenPage("/EmailConfiguration")} >
                                         <img src={Objectroup} />Email Configuration
@@ -229,18 +229,34 @@ export default function Header() {
                                         <img src={Carbontem} />Client
                                     </NavDropdown.Item>
                                 </NavDropdown>
-
                                 <div class="selecter-m mx-4">
-                                    <Select labelId="demo-simple-select-label" id="clientid" value={SelectedClient} onChange={SelectClient} >
-                                        {ClientDropdown?.map((row) => (
-                                            <MenuItem value={row?.ClientID}>{row?.Name}</MenuItem>
-                                        ))}
-                                    </Select>
+                                    {ClientDropdown &&
+                                        ClientDropdown.length > 0 ?
+                                        <>
+                                            <Select labelId="demo-simple-select-label" id="clientid" value={SelectedClient} onChange={SelectClient} >
+                                                {ClientDropdown?.map((row) => (
+                                                    <MenuItem value={row?.ClientID}>{row?.Name}</MenuItem>
+                                                ))}
+                                            </Select>
+                                        </>
+                                        :
+                                        <>
+                                            <Select labelId="demo-simple-select-label" id="clientid" value={SelectedClient} onChange={SelectClient} disabled >
+                                                {ClientDropdown?.map((row) => (
+                                                    <MenuItem value={row?.ClientID}>{row?.Name}</MenuItem>
+                                                ))}
+                                            </Select>
+                                            <div >
+                                                {
+                                                    Show && <Stack sx={{ width: '100%' }} spacing={2}>
+                                                        <Alert severity="error">   <strong> Sorry!</strong> No Client To Display</Alert>
+                                                    </Stack>
+                                                }
+                                            </div>
+                                        </>
+                                    }
                                 </div>
-
-
                             </Nav>
-
                         </Navbar.Collapse>
                         <div className="dropboxcard ml-3">
                             <a href="#" className="" onClick={OpenPopupUserDetails}>
@@ -249,13 +265,13 @@ export default function Header() {
                                 </span>
                             </a>
 
-                            <div className="userdropdown" id="id_userbox" ref={wrapperRef}>
+                            <div className="userdropdown" id="id_userbox" ref={WrapperRef}>
                                 <div className="bg-themehead">
                                     <span className="userpic us-max-110">
                                         {UserImage !== undefined ? <img src={UserImage} width="110px" alt="" /> : <img src={defaultimage} width="110px" alt="" />}
                                     </span>
                                     <div className="carduser_details">
-                                        <h4>{UserDetails!= undefined ? UserDetails.FirstName + " " + UserDetails.LastName : ""}</h4>
+                                        <h4>{UserDetails != undefined ? UserDetails.FirstName + " " + UserDetails.LastName : ""}</h4>
                                         <a href="">{UserDetails != undefined ? UserDetails.Email : ""}</a>
                                     </div>
                                 </div>
@@ -269,28 +285,28 @@ export default function Header() {
                                                 <h5>All User</h5>
                                             </div>
                                         </ListItem>
-                                      
-                                        <Divider variant="inset" component="li" />
-                                        {UserDetails == undefined?"": UserDetails.EmailAccount.map((row) => (
 
-                                        <ListItem alignItems="flex-start">
-                                            <ListItemAvatar>
-                                                <Avatar alt="Remy Sharp" />
-                                            </ListItemAvatar>
-                                            <ListItemText
-                                                primary={row.FirstName +" "+ row.LastName}
-                                                secondary={
-                                                    <React.Fragment>
-                                                       {row.Email}
-                                                    </React.Fragment>
-                                                }
-                                            />
-                                            <ul className='d-flex flexlist'>
-                                                <li><a href=''><img src={Chatmail} /><span className='orange'>10</span></a></li>
-                                                <li><a href=''><img src={Chatmail} /><span className='blue'>10</span></a></li>
-                                            </ul>
-                                        </ListItem>
-                                                ))} 
+                                        <Divider variant="inset" component="li" />
+                                        {UserDetails == undefined ? "" : UserDetails.EmailAccount.map((row) => (
+
+                                            <ListItem alignItems="flex-start">
+                                                <ListItemAvatar>
+                                                    <Avatar alt="Remy Sharp" />
+                                                </ListItemAvatar>
+                                                <ListItemText
+                                                    primary={row.FirstName + " " + row.LastName}
+                                                    secondary={
+                                                        <React.Fragment>
+                                                            {row.Email}
+                                                        </React.Fragment>
+                                                    }
+                                                />
+                                                <ul className='d-flex flexlist'>
+                                                    <li><a href=''><img src={Chatmail} /><span className='orange'>10</span></a></li>
+                                                    <li><a href=''><img src={Chatmail} /><span className='blue'>10</span></a></li>
+                                                </ul>
+                                            </ListItem>
+                                        ))}
                                         {/* <Divider variant="inset" component="li" />
                                         <ListItem alignItems="flex-start">
                                             <ListItemAvatar>
