1ea4ba1709a35b520db6e44494445c1d9198d4f9
bhumit4gvm <bhumit4gvm@gmail.com>
2022-09-30 13:26:14 +0530

    committed All Sent Emails Page
    committed All Sent Emails Page

--- src/_components/AllSentEmailsPage/AllSentEmailsPage.js
+++ src/_components/AllSentEmailsPage/AllSentEmailsPage.js
@@ -46,6 +46,10 @@ import { CommonConstants } from "../../_constants/common.constants";
 import { ResponseMessage } from "../../_constants/response.message";
 import { GetUserDetails } from "../../_helpers/Utility";
 import InfiniteScroll from "react-infinite-scroll-component";
+import ArrowRight from '@material-ui/icons/ArrowRight';
+import ArrowLeft from '@material-ui/icons/ArrowLeft';
+import Tooltip from "@material-ui/core/Tooltip";
+import timermenu from '../../images/icons/timermenu.svg';
 
 const Style = {
   position: 'absolute',
@@ -63,7 +67,7 @@ function UseOutSideAlerter(Ref) {
   useEffect(() => {
     function HandleClickOutside(Event) {
       if (Ref.current && !Ref.current.contains(Event.target)) {
-        const Element = document.getElementById("id_userboxlist")
+        const Element = document.getElementById("Userdropshow")
         Element.classList.remove("show");
       }
     }
@@ -298,6 +302,46 @@ export default function AllSentEnailsPage() {
 
   }
   // End CheckBox Code
+
+  /* start navcode */
+  
+  const NavBarClick = () => {
+    const element = document.getElementById("navclose")
+    if (element.classList.contains("opennav")) {
+      element.classList.remove("opennav");
+    }
+    else {
+      element.classList.add("opennav");
+    }
+  } 
+  /* end code*/
+
+  /* start navcode */
+  
+  const Userdropdown = () => {
+    const element = document.getElementById("Userdropshow")
+    if (element.classList.contains("show")) {
+      element.classList.remove("show");
+    }
+    else {
+      element.classList.add("show");
+    }
+  } 
+  function UseOutsideAlerter(Ref) {
+    useEffect(() => {
+      function handleClickOutside(event) {
+        if (Ref.current && !Ref.current.contains(event.target)) {
+          const element = document.getElementById("Userdropshow")
+          element.classList.remove("show");
+        }
+      }
+      document.addEventListener("mousedown", handleClickOutside);
+      return () => {
+        document.removeEventListener("mousedown", handleClickOutside);
+      };
+    }, [Ref]);
+  }
+  /* end code*/
 
   // Start Search
   const SearchBox = (e) => {
@@ -554,11 +598,15 @@ export default function AllSentEnailsPage() {
 
       <div className='bodymain'>
         <Row className='mb-columfull'>
-          <Col className='maxcontainerix'>
-            <div className='px-0 py-4 leftinbox'>
-              <div className='px-3'>
-                <Row>
-                  <Col sm={9}> <h3 className='title-h3'>All Sent Emails</h3> </Col>
+          <Col className='maxcontainerix' id="navclose">
+            <div className='closeopennav'>
+              <a className='navicons m-4' onClick={(NavBarClick)}><ArrowRight /></a>
+              <Tooltip title="All Sent Emails"><a className='m-4'><img src={timermenu} /></a></Tooltip>
+            </div>
+            <div className='navsmaller px-0 leftinbox'>
+              <div className='px-3 bgfilter'>
+                <Row> 
+                  <Col sm={9}><a className='navicons mr-2' onClick={(NavBarClick)}><ArrowLeft /></a> <h3 className='title-h3'>All Sent Emails</h3> </Col>
                   <Col sm={3}>
                     <div className="inboxnoti">
                       <NotificationsIcon />
@@ -644,7 +692,7 @@ export default function AllSentEnailsPage() {
               {
                 AllSentEmailsList.length === 0
                   ?
-                  <div id="scrollableDiv" class="listinbox mt-3">
+                  <div id="scrollableDiv" class="listinbox">
                     <InfiniteScroll
                       dataLength={AllSentEmailsList.length}
                       next={FetchMoreData}
@@ -655,7 +703,7 @@ export default function AllSentEnailsPage() {
                     </InfiniteScroll>
                   </div>
                   :
-                  <div id="scrollableDiv" class="listinbox mt-3">
+                  <div id="scrollableDiv" class="listinbox">
                     <InfiniteScroll
                       dataLength={AllSentEmailsList.length}
                       next={FetchMoreData}
@@ -675,38 +723,38 @@ export default function AllSentEnailsPage() {
                               <Col xs={1} className="pr-0">
                                 <FormControlLabel control={<Checkbox defaultChecked={SentMailsChecked.find(x => x == row._id) ? true : false} name={row._id} value={row._id} onChange={InBoxCheckBox} />} label="" />
                               </Col>
+                              <Col xs={11} className="pr-0">
+                                <Row>
+                                  <Col xs={2}>
+                                    <span className="inboxuserpic">
+                                      <img src={defaultimage} width="55px" alt="" />
+                                    </span>
+                                  </Col>
+                                  <Col xs={8}>
+                                    <h4>{row.FromEmail}</h4>
+                                    <h3>{row.Subject}</h3>
+                                  </Col>
+                                  <Col xs={2} className="pl-0">
+                                    <h6>{Moment(row.MailSentDatetime).format("LT")}</h6>
+                                    <ToggleButton className='startselct' value="check" selected={row.IsStarred} onClick={() => UpdateStarMessage(row._id)}>
+                                      <StarBorderIcon className='starone' />
+                                      <StarIcon className='selectedstart startwo' />
+                                    </ToggleButton>
+                                  </Col>
+                                </Row>
+                                <Row>
+                                  <Col xs={2} className='ja-center'>
+                                    <div className='attachfile'>
+                                      <input type="file" />
+                                      <AttachFileIcon />
+                                    </div>
+                                  </Col>
+                                  <Col xs={10}>
+                                    <p>{row.Snippet}</p>
+                                  </Col>
+                                </Row>
+                              </Col>
                             </Row>
-                            <Col xs={11} className="pr-0">
-                              <Row>
-                                <Col xs={2}>
-                                  <span className="inboxuserpic">
-                                    <img src={defaultimage} width="55px" alt="" />
-                                  </span>
-                                </Col>
-                                <Col xs={8}>
-                                  <h4>{row.FromEmail}</h4>
-                                  <h3>{row.Subject}</h3>
-                                </Col>
-                                <Col xs={2} className="pl-0">
-                                  <h6>{Moment(row.MailSentDatetime).format("LT")}</h6>
-                                  <ToggleButton className='startselct' value="check" selected={row.IsStarred} onClick={() => UpdateStarMessage(row._id)}>
-                                    <StarBorderIcon className='starone' />
-                                    <StarIcon className='selectedstart startwo' />
-                                  </ToggleButton>
-                                </Col>
-                              </Row>
-                              <Row>
-                                <Col xs={2} className='ja-center'>
-                                  <div className='attachfile'>
-                                    <input type="file" />
-                                    <AttachFileIcon />
-                                  </div>
-                                </Col>
-                                <Col xs={10}>
-                                  <p>{row.Snippet}</p>
-                                </Col>
-                              </Row>
-                            </Col>
                           </Item>
                         ))}
                       </Stack>
@@ -727,7 +775,35 @@ export default function AllSentEnailsPage() {
                     </Col>
                     <Col xs={10} className='p-0'>
                       <h5>{OpenMessage == 0 ? '' : OpenMessage.EmailAccount.FirstName}</h5>
-                      <h6>{OpenMessage == 0 ? '' : OpenMessage.ToEmail} <KeyboardArrowDownIcon /></h6>
+                      {/* <h6>{OpenMessage == 0 ? '' : OpenMessage.ToEmail} <KeyboardArrowDownIcon /></h6> */}
+                      <h6> {OpenMessage == 0 ? '' : OpenMessage.ToEmail}
+                        <a onClick={Userdropdown}>
+                          <KeyboardArrowDownIcon />
+                        </a>
+                      </h6>
+
+                      <div class="userdropall maxuserdropall" id="Userdropshow" ref={WrapperRef}>
+                        <div class="bodyuserdop textdeclist">
+                          <div className='columlistdrop'>
+                            <Row>
+                              <Col className='pr-0' sm={3} align="right"><lable>from:</lable></Col>
+                              <Col sm={9}><strong>rakesh4gvm@gmail.com</strong></Col>
+                            </Row>
+                            <Row>
+                              <Col className='pr-0' sm={3} align="right"><lable>from to:</lable></Col>
+                              <Col sm={9}>
+                                <p className='mb-0'>rakesh4gvm@gmail.com</p>
+                                <p className='mb-0'>rakesh4gvm@gmail.com</p>
+                                <p className='mb-0'>rakesh4gvm@gmail.com</p>
+                              </Col>
+                            </Row>
+                            <Row>
+                              <Col className='pr-0' sm={3} align="right"><lable>to:</lable></Col>
+                              <Col sm={9}>rakesh4gvm@gmail.com</Col>
+                            </Row>
+                          </div>
+                        </div>
+                      </div>
                     </Col>
                   </Row>
                 </Col>
