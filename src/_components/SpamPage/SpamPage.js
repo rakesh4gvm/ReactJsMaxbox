6c28ccb268720df6926520e000b5e304788e2fcc
shubhamgvm <shubham4gvm@gmail.com>
2022-11-04 17:40:41 +0530

    add templates in inbox and sent pages
    add templates in inbox and sent pages

--- src/_components/SpamPage/SpamPage.js
+++ src/_components/SpamPage/SpamPage.js
@@ -36,6 +36,10 @@ import FormControlLabel from '@mui/material/FormControlLabel';
 import downarrow from '../../images/icon_downarrow.svg';
 import Checkbox from '@mui/material/Checkbox';
 import Avatar from '@mui/material/Avatar';
+import Accordion from '@mui/material/Accordion';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 import SpamComposePage from "../SpamComposePage/SpamComposePage"
 import inboxuser1 from '../../images/avatar/1.jpg';
@@ -75,6 +79,17 @@ import "react-toastify/dist/ReactToastify.css";
 toast.configure();
 
 const Style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+const style = {
   position: 'absolute',
   top: '50%',
   left: '50%',
@@ -133,6 +148,64 @@ export default function SpamPage() {
   const [ForwardSignature, SetForwardSignature] = useState({
     Data: ""
   })
+
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const [temopen, setTemOpen] = React.useState(false);
+  const handleTemOpen = () => setTemOpen(true);
+  const handleTemClose = () => setTemOpen(false);
+  const [expanded, setExpanded] = React.useState(false);
+  const [ObjectData, SetAllObjectData] = useState([])
+  const [TemplateData, SetAllTemplateData] = useState([])
+  const [ClientData, SetClientData] = useState()
+
+  const handleChange = (panel) => (event, isExpanded) => {
+    console.log(panel);
+    setExpanded(isExpanded ? panel : false);
+  };
+
+  const SelectTemplate = () => {
+    var GetByClass = document.getElementsByClassName('active');
+    if (GetByClass.length > 0) {
+      var TemplateID = document.getElementsByClassName('active')[0].id;
+      var DivData = TemplateData.find(data => data.TemplatesID === TemplateID);
+      var BodyData = Signature.Data;
+      document.getElementById("Subject").value = DivData.Subject;
+      // var NewData = BodyData + '</br>' + DivData.BodyText;
+      var NewData = DivData.BodyText + BodyData
+      SetSignature({ Data: NewData });
+      handleTemClose()
+    } else {
+      toast.error("Please select template");
+    }
+  }
+
+  const SelectObjectTemplate = () => {
+    var GetByClass = document.getElementsByClassName('active');
+    if (GetByClass.length > 0) {
+      var ObjectionTemplateID = document.getElementsByClassName('active')[0].id;
+      var DivData = ObjectData.find(data => data.ObjectionTemplateID === ObjectionTemplateID);
+      var BodyData = Signature.Data;
+      document.getElementById("Subject").value = DivData.Subject;
+      var NewData = DivData.BodyText + BodyData
+      SetSignature({ Data: NewData });
+      handleClose()
+    } else {
+      toast.error("Please select object template");
+    }
+  }
+
+  const ActiveClass = (panel) => () => {
+    const element = document.getElementById(panel)
+    const elementcs = document.getElementsByClassName("active")
+    if (elementcs.length > 0) {
+      for (var i = elementcs.length - 1; i >= 0; i--) {
+        elementcs[i].classList.remove("active");
+      }
+    }
+    element.classList.add("active");
+  }
 
   useEffect(() => {
     document.title = 'Spam | MAXBOX';
@@ -826,6 +899,85 @@ export default function SpamPage() {
 
     }
   });
+  /* template option */
+  Froalaeditor.RegisterCommand('TemplatesOptions', {
+    title: 'Templates Option',
+    type: 'dropdown',
+    focus: false,
+    undo: false,
+    className: 'tam',
+    refreshAfterCallback: true,
+    // options: EditorVariableNames(),
+    options: {
+      'opt1': 'Objections',
+      'opt2': 'Templates'
+    },
+    callback: function (cmd, val) {
+      var editorInstance = this;
+      if (val == "opt1") {
+
+        var Data = {
+          ClientID: ClientID,
+          UserID: UserID,
+        };
+        const ResponseApi = Axios({
+          url: CommonConstants.MOL_APIURL + "/objection_template/ObjectionTemplateGetAll",
+          method: "POST",
+          data: Data,
+        });
+        ResponseApi.then((Result) => {
+          if (Result.data.StatusMessage == ResponseMessage.SUCCESS) {
+            if (Result.data.PageData.length > 0) {
+              setExpanded(false)
+              SetAllObjectData(Result.data.PageData)
+              setOpen(true);
+            } else {
+              toast.error(Result?.data?.Message);
+            }
+          } else {
+            SetAllObjectData('');
+            toast.error(Result?.data?.Message);
+          }
+        });
+        // editorInstance.html.insert("{" + val + "}");
+      }
+      if (val == "opt2") {
+        var Data = {
+          ClientID: ClientID,
+          UserID: UserID,
+        };
+        const ResponseApi = Axios({
+          url: CommonConstants.MOL_APIURL + "/templates/TemplateGetAll",
+          method: "POST",
+          data: Data,
+        });
+        ResponseApi.then((Result) => {
+          if (Result.data.StatusMessage == ResponseMessage.SUCCESS) {
+            if (Result.data.PageData.length > 0) {
+              setExpanded(false);
+              SetAllTemplateData(Result.data.PageData)
+              setTemOpen(true);
+            } else {
+              toast.error(Result?.data?.Message);
+            }
+          } else {
+            SetAllTemplateData('');
+            toast.error(Result?.data?.Message);
+          }
+        });
+
+        // editorInstance.html.insert("{" + val + "}");
+      }
+    },
+    // Callback on refresh.
+    refresh: function ($btn) {
+
+    },
+    // Callback on dropdown show.
+    refreshOnShow: function ($btn, $dropdown) {
+    }
+  });
+  /* end template option */
   Froalaeditor.RegisterCommand('moreMisc', {
     title: '',
     type: 'dropdown',
@@ -851,7 +1003,7 @@ export default function SpamPage() {
     quickInsertEnabled: false,
     placeholderText: 'Edit Your Content Here!',
     charCounterCount: false,
-    toolbarButtons: [['SendReply', 'Sendoption', 'fontSize', 'insertFile', 'insertImage', 'insertLink'], ['Delete', 'moreMisc']],
+    toolbarButtons: [['SendReply', 'Sendoption', 'fontSize', 'insertFile', 'insertImage', 'insertLink', 'TemplatesOptions'], ['Delete', 'moreMisc']],
     imageUploadURL: CommonConstants.MOL_APIURL + "/client/upload_image",
     imageUploadRemoteUrls: false,
   }
@@ -1083,8 +1235,103 @@ export default function SpamPage() {
   UseOutsideAlerter(WrapperRef);
 
   return (
+
     <>
+
       <div>
+
+        <Modal className="modal-lister"
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <div className='m-head'>
+              <Typography id="modal-modal-title" variant="h4" component="h4">
+                Select Objection
+              </Typography>
+            </div>
+            <div className='m-body'>
+              <div className='listcardman'>
+                {ObjectData?.length > 0 && ObjectData?.map((row, index) => (
+                  <div className='cardtemplate' onClick={ActiveClass(row.ObjectionTemplateID)} id={row.ObjectionTemplateID} >
+                    <Typography className='upperlable' sx={{ width: '33%', flexShrink: 0 }}>{row.Subject}</Typography>
+                    <Accordion className='activetemplate' expanded={expanded === row.ObjectionTemplateID} onChange={handleChange(row.ObjectionTemplateID)}>
+                      <AccordionSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls="panel2bh-content"
+                        id="panel2bh-header"
+                      >
+                      </AccordionSummary>
+                      <AccordionDetails >
+                        <Typography >
+                          {parse(row.BodyText)}
+                        </Typography>
+                      </AccordionDetails>
+                    </Accordion>
+                  </div>
+
+                ))}
+
+
+              </div>
+
+            </div>
+            <div className='m-fotter' align="right">
+              <ButtonGroup variant="text" aria-label="text button group">
+                <Button variant="contained btn btn-orang smallbtn mr-3" onClick={handleClose}> Cancel</Button>
+                <Button variant="contained btn btn-primary smallbtn" onClick={SelectObjectTemplate}> Select</Button>
+              </ButtonGroup>
+            </div>
+          </Box>
+        </Modal>
+
+        <Modal className="modal-lister"
+          open={temopen}
+          onClose={handleTemClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <div className='m-head'>
+              <Typography id="modal-modal-title" variant="h4" component="h4">
+                Select Template
+              </Typography>
+            </div>
+            <div className='m-body'>
+              <div className='listcardman'>
+
+                {TemplateData?.length > 0 && TemplateData?.map((row, index) => (
+                  <div className='cardtemplate' onClick={ActiveClass(row.TemplatesID)} id={row.TemplatesID} >
+                    <Typography className='upperlable' sx={{ width: '33%', flexShrink: 0 }}>{row.Subject}</Typography>
+                    <Accordion className='activetemplate' expanded={expanded === row.TemplatesID} onChange={handleChange(row.TemplatesID)}>
+                      <AccordionSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls="panel2bh-content"
+                        id="panel2bh-header"
+                      >
+                      </AccordionSummary>
+                      <AccordionDetails >
+                        <Typography >
+                          {parse(row.BodyText)}
+                        </Typography>
+                      </AccordionDetails>
+                    </Accordion>
+                  </div>
+                ))}
+              </div>
+
+            </div>
+            <div className='m-fotter' align="right">
+              <ButtonGroup variant="text" aria-label="text button group">
+                <Button variant="contained btn btn-orang smallbtn mr-3" onClick={handleTemClose}> Cancel</Button>
+                <Button variant="contained btn btn-primary smallbtn" onClick={SelectTemplate}> Select</Button>
+              </ButtonGroup>
+            </div>
+          </Box>
+        </Modal>
+
         <Modal className="modal-pre"
           open={DeletePopModel}
           onClose={CloseDeletePopModel}
