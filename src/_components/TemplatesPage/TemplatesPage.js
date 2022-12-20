4b46a7281a251dbd5653e5c03fa46a0b91508833
ModiJenish <jenish4gvm@gmail.com>
2022-12-20 17:35:47 +0530

    change calculation

--- src/_components/TemplatesPage/TemplatesPage.js
+++ src/_components/TemplatesPage/TemplatesPage.js
@@ -326,7 +326,8 @@ export default function TemplatesListPage() {
                             <TableCell><img src={SmallSend} /> {row.IsSentCount}</TableCell>
                             <TableCell><img src={MailMultipal} /> {row.IsOpenCount}</TableCell>
                             {/* <TableCell><img src={InboxRounded} /> Replies</TableCell> */}
-                            <TableCell>{parseInt(row.IsOpenCount) > 0 ? (parseInt(row.IsSentCount) / parseInt(row.IsOpenCount)) * 100 : 0 } % </TableCell>
+                            
+                            <TableCell>{parseInt(row.IsSentCount) > 0 ? (parseInt(row.IsOpenCount) / parseInt(row.IsSentCount)) * 100 : 0 } % </TableCell>
                             {/* <TableCell>Replies %</TableCell> */}
 
                             <TableCell align="left">
