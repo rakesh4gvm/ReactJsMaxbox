383bb7b1a8187da9cb734e29c01fa8adb3a7f2b3
bhumit4gvm <bhumit4gvm@gmail.com>
2023-01-11 17:21:49 +0530

    committed new design changes
    committed new design changes

--- src/_components/OtherInboxPage/OtherInboxPage.js
+++ src/_components/OtherInboxPage/OtherInboxPage.js
@@ -958,6 +958,7 @@ export default function OtherInboxPage(props) {
     imageUploadURL: CommonConstants.MOL_APIURL + "/client/upload_image",
     fileUploadURL: CommonConstants.MOL_APIURL + "/client/upload_file",
     imageUploadRemoteUrls: false,
+    key: 're1H1qB1A1A5C7E6F5D4iAa1Tb1YZNYAh1CUKUEQOHFVANUqD1G1F4C3B1C8E7D2B4B4=='
   }
   const ForwardHandleModelChange = (Model) => {
     SetForwardSignature({
